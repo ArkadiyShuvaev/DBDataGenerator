d1dbd1deceab34eb261c9b37b1a37d58298b7ed4
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2018-01-29 14:23:57 +0300

    Some uncommitted drafts

---
+++ ConfigFileConfigurator/src/client/client.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react"
+import {render} from "react-dom"
+
+const App = () => {
+    return (
+        <div>
+            <p>Hello world!</p>
+        </div>
+    );
+}
+const elementById = document.getElementById("app");
+render(<App />, elementById);
---
+++ ConfigFileConfigurator/src/server/server.ts
@@ -0,0 +1,11 @@
+﻿import * as http from "http";
+import {Router} from "./Router";
+
+const port = process.env.port || 1337;
+
+//const router = new Router();
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
